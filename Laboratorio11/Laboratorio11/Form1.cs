a70587e67770c29fbc98351764baa61d8510a0d6
anelg <anelg@LAPATRONA>
2025-10-19 16:02:10 -0500

    Laboratorio 11 listo

---
+++ Laboratorio11/Laboratorio11/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Laboratorio11
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void IblHelloWorld_Click(object sender, EventArgs e)
+        {
+            ;
+        }
+
+        private void btnClickThis_Click(object sender, EventArgs e)
+        {
+            IblHelloWorld.Text = "Hello, World";
+        }
+    }
+}
