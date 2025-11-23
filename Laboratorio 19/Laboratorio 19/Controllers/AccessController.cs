57968d2d1bd32fc7906e4d71ce3e37eb1723655d
anelg <anelg@LAPATRONA>
2025-11-23 12:36:07 -0500

    lab 19 listo

---
+++ Laboratorio 19/Laboratorio 19/Controllers/AccessController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Laboratorio_19.Models.WS;
+
+
+
+namespace Laboratorio_19.Controllers
+{
+    public class AccessController : ApiController
+    {
+
+    [HttpGet]
+        public Reply HelloWorld()
+        {
+            Reply oR = new Reply();
+            oR.result = 1;
+            oR.message = "Mi hello world en API";
+
+            return oR;
+        }
+    }
+}
+    
+
+
+
+
+
